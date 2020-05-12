c0528178d4f32c793e17d7efec6ddafd74bcf881
Florian Schmidt <florian.schmidt@platis.de>
2020-05-12 18:36:09 +0200

    First commit after relocation

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,3 @@
-/*
-plugins {
-    // id("kotlin-1.3-compat")
-    id("org.jetbrains.kotlin.js") version "1.3.70"//-eap-184"
-    // id("org.jetbrains.kotlin.frontend")
-    id ("com.github.hierynomus.license") version "0.15.0"
-   // `publishing-conventions`
-    `maven-publish`
-    maven
-    //kotlin("plugin.serialization") version "1.3.70"
-}
-*/
 plugins {
     //java
     kotlin("multiplatform") version "1.3.70"
@@ -31,73 +19,10 @@ repositories {
     mavenCentral()
     jcenter()
 }
-/*
-dependencies {
 
-    implementation(kotlin("stdlib-js"))
-
-    // evoleq
-    implementation(Config.Dependencies.evoleqCoreJs)
-    implementation(Config.Dependencies.dynamicsJs)
-    implementation(Config.Dependencies.configurationsJs)
-    implementation("org.evoleq:mathcat-result-js:1.0.0")
-
-    //React, React DOM + Wrappers
-    implementation("org.jetbrains:kotlin-react:16.13.0-pre.94-kotlin-1.3.70")
-    implementation("org.jetbrains:kotlin-react-dom:16.13.0-pre.94-kotlin-1.3.70")
-    implementation("org.jetbrains:kotlin-react-router-dom:4.3.1-pre.70-kotlin-1.3.21")
-    //implementation ("org.jetbrains:kotlin-react-redux:5.0.7-pre.70-kotlin-1.3.21")
-    //implementation ("org.jetbrains:kotlin-redux:4.0.0-pre.70-kotlin-1.3.21")
-    implementation(npm("react", "16.13.1"))
-    implementation(npm("react-dom", "16.13.1"))
-    implementation(npm("react-router-dom"))
-
-
-
-    //implementation(npm("text-encoding", "0.7.0"))
-    //implementation(npm("abort-controller", "3.0.0"))
-
-}
-
-*/
-/*
-kotlin{
-    target{
-        browser {
-            dceTask {
-                keep("ktor-ktor-io.\$\$importsForInline\$\$.ktor-ktor-io.io.ktor.utils.io")
-            }
-        }
-    }
-}
-*/
 kotlin {
-    /* Targets configuration omitted.
-    *  To find out how to configure the targets, please follow the link:
-    *  https://kotlinlang.org/docs/reference/building-mpp-with-gradle.html#setting-up-targets */
-    /*
-    jvm().compilations["main"].defaultSourceSet {
-        dependencies {
-            implementation(kotlin("stdlib-jdk8"))
-            implementation(kotlin("reflect"))
-            implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.5")
-        }
-    }
-
-     */
-    /*
-    // JVM-specific tests and their dependencies:
-    jvm().compilations["test"].defaultSourceSet {
-        dependencies {
-            implementation(kotlin("test-junit"))
-        }
-    }
-
-     */
-
     js().compilations["main"].defaultSourceSet  {
         dependencies {
-            //implementation(kotlin("js"))
             implementation(kotlin("reflect"))
             implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-js:1.3.5")
             implementation(kotlin("stdlib-js"))
@@ -112,13 +37,10 @@ kotlin {
             implementation("org.jetbrains:kotlin-react:16.13.0-pre.94-kotlin-1.3.70")
             implementation("org.jetbrains:kotlin-react-dom:16.13.0-pre.94-kotlin-1.3.70")
             implementation("org.jetbrains:kotlin-react-router-dom:4.3.1-pre.70-kotlin-1.3.21")
-            //implementation ("org.jetbrains:kotlin-react-redux:5.0.7-pre.70-kotlin-1.3.21")
-            //implementation ("org.jetbrains:kotlin-redux:4.0.0-pre.70-kotlin-1.3.21")
             implementation(npm("react", "16.13.1"))
             implementation(npm("react-dom", "16.13.1"))
             implementation(npm("react-router-dom"))
         }
-        /* ... */
     }
     js().compilations["test"].defaultSourceSet {/* ... */ }
 
--- src/jsMain/kotlin/org/evoleq/reakt/reakt.kt
+++ src/jsMain/kotlin/org/evoleq/reakt/reakt.kt
@@ -11,33 +11,34 @@ import react.RProps
 import react.RState
 
 
-open class FunctionalReaktProps<Data: RState> (
+open class FunctionalReaktProps<Data : RState>(
     open var id: ID,
     open var data: Data,
     open var scope: CoroutineScope = CoroutineScope(Job()),
-    open var forceUpdate: (ID,Data)->Boolean = {_,_ ->false},
-    open var updateParent:  (ID, suspend CoroutineScope.(Data)->Data)->Unit = {_,_->Unit}
+    open var forceUpdate: (ID, Data) -> Boolean = { _, _ -> false },
+    open var updateParent: (ID, suspend CoroutineScope.(Data) -> Data) -> Unit = { _, _ -> Unit }
 ) : RProps
 
 
 abstract class FunctionalReaktComponent<Data : RState>(properties: FunctionalReaktProps<Data>) : RComponent<FunctionalReaktProps<Data>, Data>(properties) {
-    init{
+    init {
         state = properties.data
     }
+    
     @EvoleqDsl
-    fun update(senderId: ID, update: suspend CoroutineScope.(Data)->Data): Evolving<Data>  = props.scope.parallel {
-        with(onUpdate(senderId,update(props.data))) data@{
-            if(props.forceUpdate(senderId,this@data)) {
-                console.log("forcing update")
+    fun update(senderId: ID, update: suspend CoroutineScope.(Data) -> Data): Evolving<Data> = props.scope.parallel {
+        with(onUpdate(senderId, update(props.data))) data@{
+            if (props.forceUpdate(senderId, this@data)) {
                 props.data = this@data
                 forceUpdate()
             }
             this
         }
     }
+    
     @EvoleqDsl
     open fun onUpdate(senderId: ID, data: Data): Data {
-        props.updateParent(senderId){data}
+        props.updateParent(senderId) { data }
         return data
     }
 }
